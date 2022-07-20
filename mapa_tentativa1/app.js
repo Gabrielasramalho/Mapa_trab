467d66ae55ef64b542de4488e781f29de90927ec
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2022-07-20 19:00:28 -0300

    att

--- mapa_tentativa1/app.js
+++ mapa_tentativa1/app.js
@@ -5,5 +5,11 @@ fetch('img/mapaGeral.svg')
     .then(response => response.text())
     .then(response => {
      mapaConteudo.innerHTML = response
-     getActions();
+     
     });  
+
+//declarar os dados Json
+//relacionar os códigos do Json com os code do SVG
+//criar galeria de img com as descrições e linkar o json pra galeria 
+// ao clique no patrimonio a pessoal, com um código ancora. será que o a vai? para a parte da galeria.
+
