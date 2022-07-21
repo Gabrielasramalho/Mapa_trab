c23c5f80687845a03901043be9a7df1570c33a9e
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2022-07-20 22:39:10 -0300

    att

--- mapa_tentativa1/app.js
+++ mapa_tentativa1/app.js
@@ -5,11 +5,23 @@ fetch('img/mapaGeral.svg')
     .then(response => response.text())
     .then(response => {
      mapaConteudo.innerHTML = response
+         console.log(mapaConteudo)
      
+         let patrimonios = mapaConteudo.querySelectorAll("#patrimonios circle,#patrimonios rect")
+          console.log(patrimonios)  
+         for(patrimonio of patrimonios) {
+            patrimonio.addEventListener( 
+               // "click", mostraExplicacao 
+            )
+         }
     });  
-
+  
+   
+     
+    //criar funcção
 //declarar os dados Json
+
 //relacionar os códigos do Json com os code do SVG
-//criar galeria de img com as descrições e linkar o json pra galeria 
+//
 // ao clique no patrimonio a pessoal, com um código ancora. será que o a vai? para a parte da galeria.
 
