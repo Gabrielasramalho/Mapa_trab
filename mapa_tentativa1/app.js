c32bf32313a6d125e896a1faaba57fe96847a99b
Gabrielasramalho <99361220+Gabrielasramalho@users.noreply.github.com>
2022-07-21 16:53:09 -0300

    att

--- mapa_tentativa1/app.js
+++ mapa_tentativa1/app.js
@@ -1,32 +1,34 @@
+//               OUVIR EVENTO DE CLICK NOS PATRIMÔNIOS DO MAPA                   //
 
-//usar a função fetch para checar a img no 
+//checar o arquivo img, converter ele para texto, depois incluir isso no HTML para ficar como código. Daclarar a classe de objetos que responderão ao evento, neste caso são os circle e react dentro do id Patrimonios, colocar o js para ouvir o evento de click, ao ouvir o click ativar as funções, neste caso alterarPopup
 
-fetch('img/mapaGeral.svg')
-    .then(response => response.text())
-    .then(response => {
-     mapaConteudo.innerHTML = response
-         console.log(mapaConteudo)
-     
-         let patrimonios = mapaConteudo.querySelectorAll("#patrimonios circle,#patrimonios rect")
-          console.log(patrimonios)  
-         for(patrimonio of patrimonios) {
-            patrimonio.addEventListener( 
-                "click", alterarPopup
-            )
-         }
-    });  
-  
-    //Criar função para alterar o estado do popup
-function alterarPopup() { 
-   //pega o elemento da classe popup1 e cria uma classe ativa
-   document.getElementById("popup-1").classList.toggle("ativa");
+fetch("img/mapaGeral.svg")
+  .then((response) => response.text())
+  .then((response) => {
+    mapaConteudo.innerHTML = response;
+
+    let patrimonios = mapaConteudo.querySelectorAll(
+      "#patrimonios circle,#patrimonios rect, #patrimonios #misto"
+    );
+
+    for (patrimonio of patrimonios) {
+      patrimonio.addEventListener("click", alterarPopup);
     }
+  });
 
-     
-    //criar funcção
+//      MOSTRAR E ESCONDER POPUP COM AS INFOS COMPLEMENTARES DO MAPA                 //
+
+//Criar função para alterar o estado do popup
+//pega o elemento da classe popup1 e substitui por uma classe ativa no css através do Toggle
+function alterarPopup() {  
+  document.getElementById("popup-1").classList.toggle("ativa");
+}
+
+
+//criar funcção
 //declarar os dados Json
 
 //relacionar os códigos do Json com os code do SVG
 //
-// ao clique no patrimonio a pessoal, com um código ancora. será que o a vai? para a parte da galeria.
 
+//galeria de fotos:
